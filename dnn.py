3c6f435534cac8287499c3cdd82c16182fdfe902
cprieels <cprieels@lxplus.cern.ch>
2019-11-05 16:02:46 +0100

    New classifiers, variables and global improvements

--- dnn.py
+++ dnn.py
@@ -4,8 +4,14 @@ from os.path import isfile
 from keras.models import Sequential
 from keras.layers import Dense, Activation
 from keras.regularizers import l2
-from keras.optimizers import SGD
+from keras.optimizers import SGD, RMSprop
 import optparse
+
+# ===========================================
+# Arguments to be updated
+# ===========================================
+
+variables = ["PuppiMET_pt", "MET_pt", "TkMET_pt", "mT2", "dphill", "dphillmet", "Lepton_pt[0]", "Lepton_pt[1]", "Lepton_eta[0]", "Lepton_eta[1]", "Lepton_phi[0]", "Lepton_phi[1]", "mll", "njet", "mtw1", "mtw2"]
 
 # ===========================================
 # Argument parser
@@ -17,8 +23,6 @@ parser.add_option('-b', '--background', action='store', type=str, dest='backgrou
 
 signal     = opts.signal
 background = opts.background
-
-variables = ["mT2", "PuppiMET_pt", "dphillmet"]
     
 # ===========================================
 # Setup TMVA
@@ -41,36 +45,43 @@ signalFile = TFile.Open(signal)
 signalTree = signalFile.Get('Events')
 backgroundFile = TFile.Open(background)
 backgroundTree = backgroundFile.Get('Events')
+
 dataloader = TMVA.DataLoader('dataset')
 
-for branch in signalTree.GetListOfBranches():
-    if branch.GetName() in variables:
-        dataloader.AddVariable(branch.GetName())
+for variable in variables:
+    dataloader.AddVariable(variable)
+
 dataloader.AddSignalTree(signalTree, 1.0)
 dataloader.AddBackgroundTree(backgroundTree, 1.0)
 dataloader.PrepareTrainingAndTestTree(TCut(''),
-                                      'nTrain_Signal=4000:nTrain_Background=4000:SplitMode=Random:NormMode=NumEvents:!V')
+                                      'nTrain_Signal=1000:nTrain_Background=100000:SplitMode=Random:NormMode=NumEvents:!V')
 
 # ===========================================
 # Generate model
 # ===========================================
 model = Sequential()
-model.add(Dense(64, activation='relu', input_dim=3))
+model.add(Dense(10, activation='relu', input_dim=len(variables)))
+model.add(Dense(10, activation='relu'))
+model.add(Dense(10, activation='relu'))
+model.add(Dense(10, activation='relu'))
+model.add(Dense(10, activation='relu'))
 model.add(Dense(2, activation='softmax'))
 
 # Set loss and optimizer
-model.compile(loss='categorical_crossentropy',
-              optimizer=SGD(lr=0.01), metrics=['accuracy', ])
+model.compile(loss='categorical_crossentropy', 
+              optimizer=RMSprop(), metrics=['accuracy', 'mse'])
 
 # Store model to file
 model.save('model.h5')
 model.summary()
 
 # Book methods
-#factory.BookMethod(dataloader, TMVA.Types.kFisher, 'Fisher',
-#                   '!H:!V:Fisher:VarTransform=D,G')
+factory.BookMethod(dataloader, TMVA.Types.kFisher, 'Fisher',
+                   '!H:!V:Fisher:VarTransform=D,G')
 factory.BookMethod(dataloader, TMVA.Types.kPyKeras, 'PyKeras',
                    'H:!V:VarTransform=D,G:FilenameModel=model.h5:NumEpochs=20:BatchSize=32')
+factory.BookMethod(dataloader, TMVA.Types.kLikelihood, 'LikelihoodD',
+                     '!H:!V:!TransformOutput:PDFInterpol=Spline2:NSmoothSig[0]=20:NSmoothBkg[0]=20:NSmooth=5:NAvEvtPerBin=50:VarTransform=Decorrelate')
 
 # ===========================================
 # Run training, test and evaluation
