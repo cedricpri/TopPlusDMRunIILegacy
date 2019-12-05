d9f63bcaabb76a59ae9be57b13af4462fd316f6b
cprieels <cprieels@lxplus.cern.ch>
2019-12-05 16:00:29 +0100

    Added new variables

--- dnn.py
+++ dnn.py
@@ -14,23 +14,24 @@ import os, fnmatch
 # ===========================================
 
 #DESY variables
-thetal0 = "TMath::Cos(2*TMath::ATan(exp(-Lepton_eta[0])))"
-thetal1 = "TMath::Cos(2*TMath::ATan(exp(-Lepton_eta[1])))"
-thetab = "TMath::Cos(2*TMath::ATan(exp(-CleanJet_eta[0])))"
-thetab2 = "TMath::Cos(2*TMath::ATan(exp(-CleanJet_eta[1])))"
+thetal0 = "TMath::Cos(2*TMath::ATan(exp(-Lepton0_eta)))"
+thetal1 = "TMath::Cos(2*TMath::ATan(exp(-Lepton1_eta)))"
+thetab = "TMath::Cos(2*TMath::ATan(exp(-CleanbJet0_eta)))"
+thetab2 = "TMath::Cos(2*TMath::ATan(exp(-CleanbJet1_eta)))"
 thetal0l1 = thetal0 + "*" + thetal1
 thetal0b = thetal0 + "*" + thetab
 thetal1b = thetal1 + "*" + thetab
 
-variables = ["PuppiMET_pt", "MET_pt", "TkMET_pt", "mT2", "dphill", "dphillmet", "Lepton_pt[0]", "Lepton_pt[1]", "mll", "njet", "mtw1", "mtw2", "mth", "Lepton_eta[0]-Lepton_eta[1]", "Lepton_phi[0]-Lepton_phi[1]", "thetal0l1 := "+thetal0l1, "thetal0b := "+thetal0b, "thetal1b := "+thetal1b]
-baseDir = "/eos/cms/store/group/phys_higgs/cmshww/amassiro/HWWNano/Summer16_102X_nAODv4_Full2016v5/MCl1loose2016v5__MCCorr2016v5__l2loose__l2tightOR2016v5/"
+variables = ["PuppiMET_pt", "MET_pt", "TkMET_pt", "mT2", "dphill", "dphillmet", "Lepton0_pt", "Lepton1_pt", "mll", "njet", "mtw1", "mtw2", "mth", "Lepton0_eta-Lepton1_eta", "Lepton0_phi-Lepton1_phi", "thetal0l1 := "+thetal0l1, "thetal0b := "+thetal0b, "thetal1b := "+thetal1b]
+#baseDir = "/eos/cms/store/group/phys_higgs/cmshww/amassiro/HWWNano/Summer16_102X_nAODv4_Full2016v5/MCl1loose2016v5__MCCorr2016v5__l2loose__l2tightOR2016v5/"
+baseDir = os.getcwd() + "/rootfiles/"
 
 # ===========================================
 # Argument parser
 # ===========================================
 parser = optparse.OptionParser(usage='usage: %prog [opts] FilenameWithSamples', version='%prog 1.0')
-parser.add_option('-s', '--signal', action='store', type=str, dest='signal', default=baseDir+'nanoLatino_DMScalar_ttbar01j_Mchi1_Mphi100_Private2019.root', help='Name of the signal file to be used')
-parser.add_option('-b', '--background', action='store', type=str, dest='background', default=baseDir+'nanoLatino_TTTo2L2Nu__part0.root', help='Name of the background file samples can be found')
+parser.add_option('-s', '--signal', action='store', type=str, dest='signal', default=baseDir+'nanoLatino_DMScalar_ttbar01j_Mchi1_Mphi100_Private2019_dnn.root', help='Name of the signal file to be used')
+parser.add_option('-b', '--background', action='store', type=str, dest='background', default=baseDir+'nanoLatino_TTTo2L2Nu__part0_dnn.root', help='Name of the background file samples can be found')
 (opts, args) = parser.parse_args()
 
 signal     = opts.signal
@@ -44,7 +45,7 @@ TMVA.PyMethodBase.PyInitialize()
 output = TFile.Open('TMVA.root', 'RECREATE')
 
 factory = TMVA.Factory('TMVAClassification', output,
-                       '!V:!Silent:Color:DrawProgressBar:Transformations=D,G:AnalysisType=Classification')
+                       '!V:!Silent:Color:DrawProgressBar:AnalysisType=Classification')
 
 # ===========================================
 # Load data
@@ -60,7 +61,6 @@ for variable in variables:
 #For now, we assume there is only one signal file
 signalFile = TFile.Open(signal)
 signalTree = signalFile.Get('Events')
-
 dataloader.AddSignalTree(signalTree, 1.0)
 
 #However we have several background files, so let's read them in a loop
@@ -76,29 +76,8 @@ canvas.cd()
 
 chain.Draw("Lepton_pt[1]")
 backgroundTree = chain.GetTree()
-
-"""
-# TEST
-thetabSignal = TH1F('thetabSignal', 'thetabSignal', 50, -2, 2)
-thetab2Signal = TH1F('thetab2Signal', 'thetab2Signal', 50, -2, 2)
-thetabBkg = TH1F('thetabBkg', 'thetabBkg', 50, -2, 2)
-thetab2Bkg = TH1F('thetab2Bkg', 'thetab2Bkg', 50, -2, 2)
-
-signalTree.Draw(thetab+">>thetabSignal")
-signalTree.Draw(thetab2+">>thetab2Signal")
-backgroundTree.Draw(thetab+">>thetabBkg")
-backgroundTree.Draw(thetab2+">>thetab2Bkg")
-thetabSignal.SetLineColor(ROOT.kRed)
-
-thetabBkg.Scale(1/(thetabBkg.Integral()))
-thetabSignal.Scale(1/(thetabSignal.Integral()))
-
-thetabBkg.Draw()
-thetabSignal.Draw("SAME")
-# END TEST
-"""
-
 dataloader.AddBackgroundTree(backgroundTree, 1.0)
+
 dataloader.PrepareTrainingAndTestTree(TCut(''),
                                       'nTrain_Signal=2000:nTrain_Background=2000:nTest_Signal=2000:nTest_Background=2000:SplitMode=Random:NormMode=NumEvents:!V')
 
@@ -121,13 +100,15 @@ model.compile(loss='categorical_crossentropy',
 model.save('model.h5')
 model.summary()
 
-# Book methods
+# Book method
+factory.BookMethod(dataloader, TMVA.Types.kBDT, 'BDT',
+                   '!H:!V')
 factory.BookMethod(dataloader, TMVA.Types.kFisher, 'Fisher',
-                   '!H:!V:Fisher:VarTransform=D,G')
+                   '!H:!V:Fisher')
 factory.BookMethod(dataloader, TMVA.Types.kPyKeras, 'PyKeras',
-                   'H:!V:VarTransform=D,G:FilenameModel=model.h5:NumEpochs=20:BatchSize=32')
+                   'H:!V:FilenameModel=model.h5:NumEpochs=20:BatchSize=32')
 factory.BookMethod(dataloader, TMVA.Types.kLikelihood, 'LikelihoodD',
-                   '!H:!V:!TransformOutput:PDFInterpol=Spline2:NSmoothSig[0]=20:NSmoothBkg[0]=20:NSmooth=5:NAvEvtPerBin=50:VarTransform=Decorrelate')
+                   '!H:!V:!TransformOutput:PDFInterpol=Spline2:NSmoothSig[0]=20:NSmoothBkg[0]=20:NSmooth=5:NAvEvtPerBin=50')
 
 # ===========================================
 # Run training, test and evaluation
--- getReady.py
+++ getReady.py
@@ -41,12 +41,70 @@ def createTree(filename):
     outputTree = TTree("Events", "New events tree")
 
     #Set the variables we want to keep
-    Lepton_pt = array("f", [0.])
-    Lepton_eta = array("f", [0.])
+    #Leptons
+    Lepton0_pt  = array("f", [0.])
+    Lepton1_pt  = array("f", [0.])
+    Lepton0_eta = array("f", [0.])
+    Lepton1_eta = array("f", [0.])
+    Lepton0_phi = array("f", [0.])
+    Lepton1_phi = array("f", [0.])
+
+    #Jets
+    CleanJet0_pt  = array("f", [0.])
+    CleanJet1_pt  = array("f", [0.])
+    CleanJet0_eta = array("f", [0.])
+    CleanJet1_eta = array("f", [0.])
+    njet          = array("f", [0.])
+
+    #bJets
+    CleanbJet0_pt  = array("f", [0.])
+    CleanbJet1_pt  = array("f", [0.])
+    CleanbJet0_eta = array("f", [0.])
+    CleanbJet1_eta = array("f", [0.])
+    nbjet          = array("f", [0.])
+
+    #Additional variables
+    PuppiMET_pt = array("f", [0.])
+    MET_pt      = array("f", [0.])
+    TkMET_pt    = array("f", [0.])
+    mT2         = array("f", [0.])
+    dphill      = array("f", [0.])
+    dphillmet   = array("f", [0.])
+    mll         = array("f", [0.])
+    mtw1        = array("f", [0.])
+    mtw2        = array("f", [0.])
+    mth         = array("f", [0.])
 
     #Set the branches
-    outputTree.Branch("Lepton_pt", Lepton_pt, "Lepton_pt/F")
-    outputTree.Branch("Lepton_eta", Lepton_eta, "Lepton_eta/F")
+    outputTree.Branch("Lepton0_pt", Lepton0_pt, "Lepton0_pt/F")
+    outputTree.Branch("Lepton1_pt", Lepton1_pt, "Lepton1_pt/F")
+    outputTree.Branch("Lepton0_eta", Lepton0_eta, "Lepton0_eta/F")
+    outputTree.Branch("Lepton1_eta", Lepton1_eta, "Lepton1_eta/F")
+    outputTree.Branch("Lepton0_phi", Lepton0_phi, "Lepton0_phi/F")
+    outputTree.Branch("Lepton1_phi", Lepton1_phi, "Lepton1_phi/F")
+
+    outputTree.Branch("CleanJet0_pt", CleanJet0_pt, "CleanJet0_pt/F")
+    outputTree.Branch("CleanJet1_pt", CleanJet1_pt, "CleanJet1_pt/F")
+    outputTree.Branch("CleanJet0_eta", CleanJet0_eta, "CleanJet0_eta/F")
+    outputTree.Branch("CleanJet1_eta", CleanJet1_eta, "CleanJet1_eta/F")
+    outputTree.Branch("njet", njet, "njet/F")
+
+    outputTree.Branch("CleanbJet0_pt", CleanbJet0_pt, "CleanbJet0_pt/F")
+    outputTree.Branch("CleanbJet1_pt", CleanbJet1_pt, "CleanbJet1_pt/F")
+    outputTree.Branch("CleanbJet0_eta", CleanbJet0_eta, "CleanbJet0_eta/F")
+    outputTree.Branch("CleanbJet1_eta", CleanbJet1_eta, "CleanbJet1_eta/F")
+    outputTree.Branch("nbjet", nbjet, "nbjet/F")
+
+    outputTree.Branch("PuppiMET_pt", PuppiMET_pt, "PuppiMET_pt/F")
+    outputTree.Branch("MET_pt", MET_pt, "MET_pt/F")
+    outputTree.Branch("TkMET_pt", TkMET_pt, "TkMET_pt/F")
+    outputTree.Branch("mT2", mT2, "mT2/F")
+    outputTree.Branch("dphill", dphill, "dphill/F")
+    outputTree.Branch("dphillmet", dphillmet, "dphillmet/F")
+    outputTree.Branch("mll", mll, "mll/F")
+    outputTree.Branch("mtw1", mtw1, "mtw1/F")
+    outputTree.Branch("mtw2", mtw2, "mtw2/F")
+    outputTree.Branch("mth", mth, "mth/F")
 
     nEvents = 0
     for ev in inputFile.Events:
@@ -63,9 +121,63 @@ def createTree(filename):
         if ev.Lepton_pt[0] < 20. or ev.Lepton_pt[1] < 20.:
             continue
     
-        Lepton_pt = ev.Lepton_pt
-        Lepton_eta = ev.Lepton_eta
-    
+        #Leptons
+        Lepton0_pt[0] = ev.Lepton_pt[0]
+        if(len(ev.Lepton_pt) > 1):
+            Lepton1_pt[0] = ev.Lepton_pt[1]
+
+        Lepton0_eta[0] = ev.Lepton_eta[0]
+        if(len(ev.Lepton_eta) > 1):
+            Lepton1_eta[0] = ev.Lepton_eta[1]
+
+        Lepton0_phi[0] = ev.Lepton_phi[0]
+        if(len(ev.Lepton_phi) > 1):
+            Lepton1_phi[0] = ev.Lepton_phi[1]
+
+        #Jets
+        nJets = 0
+        for jet in ev.CleanJet_jetIdx:
+            if nJets < 2:
+                if nJets == 0:
+                    nJets = 1
+                    CleanJet0_pt[0] = ev.Jet_pt[jet]
+                    CleanJet0_eta[0] = ev.Jet_eta[jet]
+                if nJets == 1:
+                    nJets = 2
+                    CleanJet1_pt[0] = ev.Jet_pt[jet]
+                    CleanJet1_eta[0] = ev.Jet_eta[jet]
+
+        njet = ev.njet;
+
+        #bJets
+        nBjets = 0
+        for jet in ev.CleanJet_jetIdx:
+            if nBjets < 2:
+                if ev.Jet_btagDeepB[jet] > 0.2217 and nBjets == 0:
+                    nBjets = 1
+                    CleanbJet0_pt[0] = ev.Jet_pt[jet]
+                    CleanbJet0_eta[0] = ev.Jet_eta[jet]
+                if ev.Jet_btagDeepB[jet] > 0.2217 and nBjets == 1:
+                    nBjets = 2
+                    CleanbJet1_pt[0] = ev.Jet_pt[jet]
+                    CleanbJet1_eta[0] = ev.Jet_eta[jet]
+                elif ev.Jet_btagDeepB[jet] > 0.2217:
+                    nBjets = nBjets + 1
+
+        nbjet = nBjets
+
+        #Additional variables
+        PuppiMET_pt[0] = ev.PuppiMET_pt;
+        MET_pt[0] = ev.MET_pt;
+        TkMET_pt[0] = ev.TkMET_pt;
+        mT2[0] = ev.mT2;
+        dphill[0] = ev.dphill;
+        dphillmet[0] = ev.dphillmet;
+        mll[0] = ev.mll;
+        mtw1[0] = ev.mtw1;
+        mtw2[0] = ev.mtw2;
+        mth[0] = ev.mth;
+
         outputTree.Fill()
 
     outputTree.Write()
