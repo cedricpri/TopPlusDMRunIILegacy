48b18f4de01df1ebf6e23e84540ff903fd1f87e7
cprieels <cprieels@lxplus.cern.ch>
2020-03-19 17:03:27 +0100

    Script simplified

--- scripts/plotSignals.py
+++ scripts/plotSignals.py
@@ -1,70 +1,49 @@
-import os, fnmatch, sys
-from ROOT import TCanvas, TLegend, TFile, TProfile, TNtuple, TH1F, TH2F
+from ROOT import TCanvas, TLegend, TFile, TH1F
 
 #=============================================================================
 #SETUP
 #=============================================================================
-#Directory where the signal samples can be found
+variable = ["PuppiMET_pt", 100, 0, 500, "Puppi MET [GeV]"] #Variable name, bins, from, to, xaxis title
 signalDir = "/eos/user/c/cprieels/work/SignalsPostProcessing/Pablo/Autumn18_102X_nAODv6_Full2018v6/MCl1loose2018v6__MCCorr2018v6__l2loose__l2tightOR2018v6/"
-
-#Categorios and variables to be plotted
-categories = ["scalar", "pseudoscalar"] #Add the single top once available
-variables = [["PuppiMET_pt", 0, 200, 20, "Puppi MET [GeV]"]] #Variable name, from, to, bins, xaxis
-
-#=============================================================================
-# HELPERS
-#=============================================================================
-#Progress bar
-def updateProgress(progress):
-    barLength = 20 # Modify this to change the length of the progress bar
-    status = ""
-    if isinstance(progress, int):
-        progress = float(progress)
-    if not isinstance(progress, float):
-        progress = 0
-        status = "error: progress var must be float\r\n"
-    if progress < 0:
-        progress = 0
-        status = "Halt...\r\n"
-    if progress >= 1:
-        progress = 1
-        status = "Done!\r\n"
-    block = int(round(barLength*progress))
-    text = "\rProgress: [{0}] {1}% {2}".format( "#"*block + "-"*(barLength-block), progress*100, status)
-    sys.stdout.write(text)
-    sys.stdout.flush()
+category = "scalar"
+files = ['nanoLatino_TTbarDMJets_Dilepton_'+category+'_LO_Mchi_1_Mphi_50.root', 
+         'nanoLatino_TTbarDMJets_Dilepton_'+category+'_LO_Mchi_1_Mphi_100.root',
+         'nanoLatino_TTbarDMJets_Dilepton_'+category+'_LO_Mchi_1_Mphi_150.root',
+         'nanoLatino_TTbarDMJets_Dilepton_'+category+'_LO_Mchi_1_Mphi_200.root',
+         'nanoLatino_TTbarDMJets_Dilepton_'+category+'_LO_Mchi_1_Mphi_250.root',
+         'nanoLatino_TTbarDMJets_Dilepton_'+category+'_LO_Mchi_1_Mphi_300.root',
+         'nanoLatino_TTbarDMJets_Dilepton_'+category+'_LO_Mchi_1_Mphi_350.root',
+         'nanoLatino_TTbarDMJets_Dilepton_'+category+'_LO_Mchi_1_Mphi_400.root',
+         'nanoLatino_TTbarDMJets_Dilepton_'+category+'_LO_Mchi_1_Mphi_450.root',
+         'nanoLatino_TTbarDMJets_Dilepton_'+category+'_LO_Mchi_1_Mphi_500.root']
+         #'nanoLatino_TTbarDMJets_Dilepton_'+category+'_LO_Mchi_20_Mphi_100.root',
+         #'nanoLatino_TTbarDMJets_Dilepton_'+category+'_LO_Mchi_30_Mphi_100.root',
+         #'nanoLatino_TTbarDMJets_Dilepton_'+category+'_LO_Mchi_40_Mphi_100.root',
+         #'nanoLatino_TTbarDMJets_Dilepton_'+category+'_LO_Mchi_45_Mphi_100.root',
+         #'nanoLatino_TTbarDMJets_Dilepton_'+category+'_LO_Mchi_49_Mphi_100.root',
+         #'nanoLatino_TTbarDMJets_Dilepton_'+category+'_LO_Mchi_51_Mphi_100.root',
+         #'nanoLatino_TTbarDMJets_Dilepton_'+category+'_LO_Mchi_55_Mphi_100.root']
 
 #=============================================================================
 #GET STARTED
 #=============================================================================
-for c, category in enumerate(categories):
-    legend = TLegend(0.1, 0.65, 0.25, 0.9)
 
-    for v, variable in enumerate(variables):
-        canvas = TCanvas( 'canvas_'+str(category)+'_'+str(variable[0]), 'Signal samples', 200, 10, 700, 500 )
-        canvas.cd()
+canvas = TCanvas( 'canvas', 'Signal samples', 200, 10, 700, 500 )
+canvas.cd()
 
-    filenames = fnmatch.filter(os.listdir(signalDir), '*_'+category+'*Mchi_1_*') #For now, only mChi 1 GeV
-    for f, filename in enumerate(filenames):
-        print('Now reading file... ' + filename)
-        massPoint = "_".join(filename.split("_")[5:9]).replace('.root', '')
+for f, filename in enumerate(files):
+    print('Now reading file... ' + filename)
 
-        signalFile = TFile.Open(signalDir+filename, "read")
-        signalTree = signalFile.Get("Events")
+    signalFile = TFile.Open(signalDir+filename, "read")
+    signalTree = signalFile.Get("Events")
+        
+    histname = 'hist_'+str(f)
+    signalHist = TH1F(histname, 'Mass points distribution ('+category+')', variable[1], variable[2], variable[3])        
+    signalTree.Draw(variable[0] + ' >> ' + histname, '', 'same, goff')
 
-        for v, variable in enumerate(variables):
-            histname = 'hist_'+str(category)+'_'+str(variable[0])+'_'+str(filename)
-            signalHist = TH1F(histname, 'Mass points ' + variable[0] + ' distribution (' + category + ")", variable[3], variable[1], variable[2])
-            
-            signalTree.Draw(variable[0] + ' >> ' + histname)
-            signalHist.Scale(1./signalHist.Integral()) #Normalize the histo to unity
-            signalHist.SetLineWidth(4)
-            signalHist.SetLineColor(632+f)
-            signalHist.SetStats(0)
-            signalHist.GetXaxis().SetTitle(variable[4])
-            signalHist.Draw('same')
+    if f == 0:
+        signalHist.Draw('hist')
+    else:
+        signalHist.Draw('hist, same')
 
-            legend.AddEntry(signalHist, massPoint)
-            legend.Draw()
-            
-    canvas.SaveAs(category+'.png')
+canvas.SaveAs(category+'.png')
