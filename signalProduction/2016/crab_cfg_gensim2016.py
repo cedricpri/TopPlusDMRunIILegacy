daa3797a4075bd72e124c912b2a1600c74f53c62
cprieels <cprieels@cern.ch>
2020-08-10 13:51:40 +0200

    2016 and 2017 GEN-SIM added

--- signalProduction/2016/crab_cfg_gensim2016.py
+++ signalProduction/2016/crab_cfg_gensim2016.py
@@ -5,7 +5,7 @@ config = Configuration()
 
 ## General options for the client
 config.section_("General")
-config.General.requestName   = 'DMscalar_Dilepton_top_tWChan_Mchi1_Mphi10_RunIISummer16_GENSIM_PrivateFullProd'
+config.General.requestName   = 'DMscalar_Dilepton_top_tWChan_Mchi1_Mphi1000_RunIISummer16_GENSIM_PrivateFullProd'
 config.General.workArea = 'crab_output'
 
 ## Specific option of the job type
@@ -13,7 +13,7 @@ config.General.workArea = 'crab_output'
 config.section_("JobType")
 config.JobType.pluginName  = 'PrivateMC'
 config.JobType.psetName    = 'gensim2016.py'
-config.JobType.inputFiles = ['/afs/cern.ch/work/d/dpinna/public/forCedric/DMScalar_top_tWChan_Mchi1_Mphi10_slc6_amd64_gcc630_CMSSW_9_3_16_tarball.tar.xz']
+config.JobType.inputFiles = ['/afs/cern.ch/work/d/dpinna/public/forCedric/DMScalar_top_tWChan_Mchi1_Mphi1000_slc6_amd64_gcc630_CMSSW_9_3_16_tarball.tar.xz']
 
 ## Specific data options
 config.section_("Data")
@@ -24,7 +24,7 @@ config.Data.outputPrimaryDataset = 'CRAB_PrivateMC'
 config.Data.publication = True
 
 #config.Data.inputDataset = '/TTbarDMJets_Dilepton_scalar_LO_TuneCP5_13TeV-madgraph-mcatnlo-pythia8/RunIIAutumn18MiniAOD-rp_102X_upgrade2018_realistic_v15-v1/MINIAODSIM'
-config.Data.outputDatasetTag   = 'DMscalar_Dilepton_top_tWChan_Mchi1_Mphi10_RunIISummer16_GENSIM_PrivateFullProd'
+config.Data.outputDatasetTag   = 'DMscalar_Dilepton_top_tWChan_Mchi1_Mphi1000_RunIISummer16_GENSIM_PrivateFullProd'
 
 config.Data.publication     = True
 config.Data.outLFNDirBase = '/store/user/cprieels/'
--- signalProduction/2016/gensim2016.py
+++ signalProduction/2016/gensim2016.py
@@ -2,7 +2,7 @@
 # using: 
 # Revision: 1.19 
 # Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
-# with command line options: Configuration/GenProduction/python/EXO-RunIISummer15GS-14144-fragment.py --fileout file:EXO-RunIISummer15GS-14144.root --mc --eventcontent RAWSIM --customise SLHCUpgradeSimulations/Configuration/postLS1Customs.customisePostLS1,Configuration/DataProcessing/Utils.addMonitoring --datatier GEN-SIM --conditions MCRUN2_71_V1::All --beamspot Realistic50ns13TeVCollision --customise_commands process.source.numberEventsInLuminosityBlock = cms.untracked.uint32(25) --step GEN,SIM --magField 38T_PostLS1 --python_filename EXO-RunIISummer15GS-14144_1_cfg.py --no_exec -n 84
+# with command line options: Configuration/GenProduction/python/EXO-RunIISummer15GS-14143-fragment.py --fileout file:EXO-RunIISummer16GS-02825.root --mc --eventcontent RAWSIM --datatier GEN-SIM --conditions MCRUN2_71_V1::All --beamspot Realistic50ns13TeVCollision --customise SLHCUpgradeSimulations/Configuration/postLS1Customs.customisePostLS1 --step GEN,SIM --magField 38T_PostLS1 -n 20 --no_exec
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process('SIM')
@@ -24,7 +24,7 @@ process.load('Configuration.StandardSequences.EndOfProcess_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(84)
+    input = cms.untracked.int32(20)
 )
 
 # Input source
@@ -37,7 +37,7 @@ process.options = cms.untracked.PSet(
 # Production Info
 process.configurationMetadata = cms.untracked.PSet(
     version = cms.untracked.string('$Revision: 1.19 $'),
-    annotation = cms.untracked.string('Configuration/GenProduction/python/EXO-RunIISummer15GS-14144-fragment.py nevts:84'),
+    annotation = cms.untracked.string('Configuration/GenProduction/python/EXO-RunIISummer15GS-14143-fragment.py nevts:20'),
     name = cms.untracked.string('Applications')
 )
 
@@ -47,7 +47,7 @@ process.RAWSIMoutput = cms.OutputModule("PoolOutputModule",
     splitLevel = cms.untracked.int32(0),
     eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
     outputCommands = process.RAWSIMEventContent.outputCommands,
-    fileName = cms.untracked.string('file:EXO-RunIISummer15GS-14144.root'),
+    fileName = cms.untracked.string('file:EXO-RunIISummer16GS-02825.root'),
     dataset = cms.untracked.PSet(
         filterName = cms.untracked.string(''),
         dataTier = cms.untracked.string('GEN-SIM')
@@ -65,31 +65,22 @@ from Configuration.AlCa.GlobalTag import GlobalTag
 process.GlobalTag = GlobalTag(process.GlobalTag, 'MCRUN2_71_V1::All', '')
 
 process.generator = cms.EDFilter("Pythia8GeneratorFilter",
-	GridpackPath = cms.string('/srv/CMSSW_10_2_16_patch1/DMScalar_top_tWChan_Mchi1_Mphi10_slc6_amd64_gcc630_CMSSW_9_3_16_tarball.tar.xz'),
-				 PythiaParameters = cms.PSet(
-                parameterSets = cms.vstring(
-                    'pythia8CommonSettings', 
-                    'pythia8CP5Settings', 
-                    'pythia8PSweightsSettings', 
-                    'processParameters'
-                ),
-                processParameters = cms.vstring(
-                    'JetMatching:setMad = off', 
-                    'JetMatching:scheme = 1', 
-                    'JetMatching:merge = on', 
-                    'JetMatching:jetAlgorithm = 2', 
-                    'JetMatching:etaJetMax = 5.', 
-                    'JetMatching:coneRadius = 1.', 
-                    'JetMatching:slowJetPower = 1', 
-                    'JetMatching:qCut = 90', 
-                    'JetMatching:nQmatch = 5', 
-                    'JetMatching:nJetMax = 1', 
-                    'JetMatching:doShowerKt = off', 
-                    'Check:epTolErr = 0.0003', 
-                    'SLHA:minMassSM = 0.1'
-                ),
-                pythia8CP5Settings = cms.vstring(
-                    'Tune:pp 14', 
+    pythiaPylistVerbosity = cms.untracked.int32(1),
+    filterEfficiency = cms.untracked.double(1.0),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+				 GridpackPath = cms.string('/srv/CMSSW_7_1_43/DMScalar_top_tWChan_Mchi1_Mphi1000_slc6_amd64_gcc630_CMSSW_9_3_16_tarball.tar.xz'),
+				 #GridpackPath = cms.string('/afs/cern.ch/work/d/dpinna/public/forCedric/DMScalar_top_tWChan_Mchi1_Mphi1000_slc6_amd64_gcc630_CMSSW_9_3_16_tarball.tar.xz'),
+    PythiaParameters = cms.PSet(
+                pythia8CommonSettings = cms.vstring('Tune:preferLHAPDF = 2', 
+                    'Main:timesAllowErrors = 10000', 
+                    'Check:epTolErr = 0.01', 
+                    'Beams:setProductionScalesFromLHEF = off', 
+                    'SLHA:keepSM = on', 
+                    'SLHA:minMassSM = 1000.', 
+                    'ParticleDecays:limitTau0 = on', 
+                    'ParticleDecays:tau0Max = 10', 
+                    'ParticleDecays:allowPhotonRadiation = on'),
+                pythia8CP5Settings = cms.vstring('Tune:pp 14', 
                     'Tune:ee 7', 
                     'MultipartonInteractions:ecmPow=0.03344', 
                     'PDF:pSet=20', 
@@ -106,34 +97,35 @@ process.generator = cms.EDFilter("Pythia8GeneratorFilter",
                     'MultipartonInteractions:alphaSvalue=0.118', 
                     'MultipartonInteractions:alphaSorder=2', 
                     'TimeShower:alphaSorder=2', 
-                    'TimeShower:alphaSvalue=0.118'
-                ),
-                pythia8CommonSettings = cms.vstring(
-                    'Tune:preferLHAPDF = 2', 
-                    'Main:timesAllowErrors = 10000', 
-                    'Check:epTolErr = 0.01', 
-                    'Beams:setProductionScalesFromLHEF = off', 
-                    'SLHA:keepSM = on', 
-                    'SLHA:minMassSM = 1000.', 
-                    'ParticleDecays:limitTau0 = on', 
-                    'ParticleDecays:tau0Max = 10', 
-                    'ParticleDecays:allowPhotonRadiation = on'
-                ),
-                pythia8PSweightsSettings = cms.vstring(
-                    'UncertaintyBands:doVariations = on', 
+                    'TimeShower:alphaSvalue=0.118'),
+                pythia8PSweightsSettings = cms.vstring('UncertaintyBands:doVariations = on', 
                     'UncertaintyBands:List = {isrRedHi isr:muRfac=0.707,fsrRedHi fsr:muRfac=0.707,isrRedLo isr:muRfac=1.414,fsrRedLo fsr:muRfac=1.414,isrDefHi isr:muRfac=0.5,fsrDefHi fsr:muRfac=0.5,isrDefLo isr:muRfac=2.0,fsrDefLo fsr:muRfac=2.0,isrConHi isr:muRfac=0.25,fsrConHi fsr:muRfac=0.25,isrConLo isr:muRfac=4.0,fsrConLo fsr:muRfac=4.0,fsr_G2GG_muR_dn fsr:G2GG:muRfac=0.5,fsr_G2GG_muR_up fsr:G2GG:muRfac=2.0,fsr_G2QQ_muR_dn fsr:G2QQ:muRfac=0.5,fsr_G2QQ_muR_up fsr:G2QQ:muRfac=2.0,fsr_Q2QG_muR_dn fsr:Q2QG:muRfac=0.5,fsr_Q2QG_muR_up fsr:Q2QG:muRfac=2.0,fsr_X2XG_muR_dn fsr:X2XG:muRfac=0.5,fsr_X2XG_muR_up fsr:X2XG:muRfac=2.0,fsr_G2GG_cNS_dn fsr:G2GG:cNS=-2.0,fsr_G2GG_cNS_up fsr:G2GG:cNS=2.0,fsr_G2QQ_cNS_dn fsr:G2QQ:cNS=-2.0,fsr_G2QQ_cNS_up fsr:G2QQ:cNS=2.0,fsr_Q2QG_cNS_dn fsr:Q2QG:cNS=-2.0,fsr_Q2QG_cNS_up fsr:Q2QG:cNS=2.0,fsr_X2XG_cNS_dn fsr:X2XG:cNS=-2.0,fsr_X2XG_cNS_up fsr:X2XG:cNS=2.0,isr_G2GG_muR_dn isr:G2GG:muRfac=0.5,isr_G2GG_muR_up isr:G2GG:muRfac=2.0,isr_G2QQ_muR_dn isr:G2QQ:muRfac=0.5,isr_G2QQ_muR_up isr:G2QQ:muRfac=2.0,isr_Q2QG_muR_dn isr:Q2QG:muRfac=0.5,isr_Q2QG_muR_up isr:Q2QG:muRfac=2.0,isr_X2XG_muR_dn isr:X2XG:muRfac=0.5,isr_X2XG_muR_up isr:X2XG:muRfac=2.0,isr_G2GG_cNS_dn isr:G2GG:cNS=-2.0,isr_G2GG_cNS_up isr:G2GG:cNS=2.0,isr_G2QQ_cNS_dn isr:G2QQ:cNS=-2.0,isr_G2QQ_cNS_up isr:G2QQ:cNS=2.0,isr_Q2QG_cNS_dn isr:Q2QG:cNS=-2.0,isr_Q2QG_cNS_up isr:Q2QG:cNS=2.0,isr_X2XG_cNS_dn isr:X2XG:cNS=-2.0,isr_X2XG_cNS_up isr:X2XG:cNS=2.0}', 
                     'UncertaintyBands:nFlavQ = 4', 
                     'UncertaintyBands:MPIshowers = on', 
                     'UncertaintyBands:overSampleFSR = 10.0', 
                     'UncertaintyBands:overSampleISR = 10.0', 
                     'UncertaintyBands:FSRpTmin2Fac = 20', 
-                    'UncertaintyBands:ISRpTmin2Fac = 1'
-		        )
-		),
-    comEnergy = cms.double(13000.0),
-    maxEventsToPrint = cms.untracked.int32(1),
-    pythiaHepMCVerbosity = cms.untracked.bool(False),
-    pythiaPylistVerbosity = cms.untracked.int32(1)
+                    'UncertaintyBands:ISRpTmin2Fac = 1'),
+                processParameters = cms.vstring('JetMatching:setMad = off', 
+                    'JetMatching:scheme = 1', 
+                    'JetMatching:merge = on', 
+                    'JetMatching:jetAlgorithm = 2', 
+                    'JetMatching:etaJetMax = 5.', 
+                    'JetMatching:coneRadius = 1.', 
+                    'JetMatching:slowJetPower = 1', 
+                    'JetMatching:qCut = 90', 
+                    'JetMatching:nQmatch = 5', 
+                    'JetMatching:nJetMax = 1', 
+                    'JetMatching:doShowerKt = off', 
+                    'Check:epTolErr = 0.0003', 
+                    'SLHA:minMassSM = 0.1'),
+                parameterSets = cms.vstring('pythia8CommonSettings', 
+                    'pythia8CP5Settings', 
+                    'pythia8PSweightsSettings', 
+                    'processParameters')
+		), 
+				 comEnergy = cms.double(13000.0),
+				 maxEventsToPrint = cms.untracked.int32(1)
 )
 
 
@@ -154,19 +146,11 @@ for path in process.paths:
 
 # customisation of the process.
 
-# Automatic addition of the customisation function from Configuration.DataProcessing.Utils
-from Configuration.DataProcessing.Utils import addMonitoring 
-
-#call to customisation function addMonitoring imported from Configuration.DataProcessing.Utils
-process = addMonitoring(process)
-
 # Automatic addition of the customisation function from SLHCUpgradeSimulations.Configuration.postLS1Customs
 from SLHCUpgradeSimulations.Configuration.postLS1Customs import customisePostLS1 
 
 #call to customisation function customisePostLS1 imported from SLHCUpgradeSimulations.Configuration.postLS1Customs
 process = customisePostLS1(process)
+process.source.numberEventsInLuminosityBlock = cms.untracked.uint32(25)
 
 # End of customisation functions
-
-# Customisation from command line
-process.source.numberEventsInLuminosityBlock = cms.untracked.uint32(25)
