143ff449f9158dc330c058ba388874f841d6f8fb
cprieels <cprieels@cern.ch>
2020-10-23 12:44:44 +0200

    DY Rin-out plotter working

--- scripts/getDYRinout.py
+++ scripts/getDYRinout.py
@@ -1,6 +1,7 @@
 #Rin-out method for the DY estimation
 
 import ROOT as r
+import math
 from array import array
 
 #!!TODO: apply this method to the region of the analysis (mt2ll cut, for example)
@@ -20,7 +21,7 @@ dataObjects = ['histo_DATA;1']
 
 #MET bins used to plot the Routin factor
 metBins = ['0', '1', '2', '3', '4', '5'] #Matching the definition of the cuts in the cuts.py file: 0 is the inclusive distribution
-metValues = [-1, 10, 20, 40, 100, -1]
+metValues = [0, 10, 20, 40, 100, 200]
 
 #Additional options
 channelColors = [r.kBlack, r.kRed, r.kBlue] 
@@ -89,39 +90,72 @@ for region in ['0bjet', '1bjetOrMore']:
 #==================================================================
 #Compute the global Routin factor and scale factor in the different channels and for different met cuts
 kappa = {}
+errorKappa = {}
+outputKappa = {}
 RoutinMC = {}
+errorRoutinMC = {}
+outputRoutinMC = {}
 
 for channel in channels:
     kappa[channel] = {}
+    errorKappa[channel] = {}
+    outputKappa[channel] = {}
     RoutinMC[channel] = {}
+    errorRoutinMC[channel] = {}
+    outputRoutinMC[channel] = {}
 
     for metBin in metBins:
         RoutinMC0bjet = (yieldsMC['0bjet']['out'][channel][metBin]/yieldsMC['0bjet']['in'][channel][metBin])
+        errorRoutinMC0bjet = RoutinMC0bjet * math.sqrt((1/yieldsMC['0bjet']['out'][channel][metBin]) + (1/yieldsMC['0bjet']['in'][channel][metBin]))
         RoutinData0bjet = (yieldsData['0bjet']['out'][channel][metBin]/yieldsData['0bjet']['in'][channel][metBin])
+        errorRoutinData0bjet = RoutinData0bjet * math.sqrt((1/yieldsData['0bjet']['out'][channel][metBin]) + (1/yieldsData['0bjet']['in'][channel][metBin]))
 
+        #Statistical error only for now
         kappa[channel][metBin] = RoutinMC0bjet/RoutinData0bjet
-        RoutinMC[channel][metBin] = kappa[channel][metBin] * (yieldsData['1bjetOrMore']['out'][channel][metBin]/yieldsData['1bjetOrMore']['in'][channel][metBin])
+        errorKappa[channel][metBin] = kappa[channel][metBin] *  math.sqrt((errorRoutinMC0bjet/RoutinMC0bjet) ** 2 + (errorRoutinData0bjet/RoutinData0bjet) ** 2)
+        outputKappa[channel][metBin] = str(kappa[channel][metBin]) + " +- " + str(errorKappa[channel][metBin])
 
-print(kappa)
-print(RoutinMC)
+        RoutinMC[channel][metBin] = kappa[channel][metBin] * (yieldsData['1bjetOrMore']['out'][channel][metBin]/yieldsData['1bjetOrMore']['in'][channel][metBin])
+        errorRoutinMC[channel][metBin] = RoutinMC[channel][metBin] * math.sqrt((1/yieldsData['1bjetOrMore']['out'][channel][metBin]) ** 2 + (1/yieldsData['1bjetOrMore']['in'][channel][metBin]) ** 2 + (errorKappa[channel][metBin]/kappa[channel][metBin]) ** 2)
+        outputRoutinMC[channel][metBin] = str(RoutinMC[channel][metBin]) + " +- " + str(errorRoutinMC[channel][metBin])
+
+print(outputKappa)
+print(outputRoutinMC)
 
 #==================================================================
 #Plot the results
 
-n = len(metBins)
 canvas = r.TCanvas("canvas", "canvas")
-multiGraph = r.TMultiGraph()
+legend = r.TLegend(0.15, 0.74, 0.3, 0.85)
+r.gStyle.SetOptStat(0)
 
+h = []
 for i, channel in enumerate(channels):
-    x, y = array( 'd' ), array( 'd' )
+    h.append(r.TH1D())
 
-    for j, metBin in enumerate(metBins):
-        if j == 0 or j == len(metBins):
-            continue ## Don't plot the inclusive met cut and the last met cut
+    if(i == 0):
+        h[i].GetXaxis().SetTitle("Pf MET [GeV]")
+        h[i].GetYaxis().SetTitle("R^{out/in} = N^{out} / N^{in}")
+        h[i].SetTitle('DY Rin-out scale factor (' + str(year) + ')')
 
-        x.append(int(metValues[j]))
-        y.append(RoutinMC[channel][metBin])
+    h[i].SetBins(len(metValues) - 1, array('d', metValues))
 
+    for j in range(1, len(metValues)): #Start at 1 to avoid plotting the inclusive MET bin
+        h[i].SetBinContent(j, RoutinMC[channel][str(j)])
+        h[i].SetBinError(j, errorRoutinMC[channel][str(j)])
+        h[i].SetLineColor(channelColors[i])
+        h[i].SetLineWidth(2)
+        h[i].SetMarkerStyle(21)
+        h[i].SetMarkerColor(channelColors[i])
+        h[i].Draw("same")
+
+    legend.AddEntry(h[i], channel)
+
+legend.SetBorderSize(0)
+legend.Draw()
+canvas.SaveAs("Rinout.png")
+
+"""
     graph = r.TGraph(n, x, y)
     graph.SetTitle(channel)
     graph.SetMinimum(0)
@@ -133,11 +167,12 @@ for i, channel in enumerate(channels):
     
     multiGraph.Add(graph, "ap")
 
-multiGraph.GetXaxis().SetTitle("Pf MET [GeV]")
-multiGraph.GetYaxis().SetTitle("R^{out/in} = N^{out} / N^{in}")
-multiGraph.SetTitle('DY Rin-out scale factor (' + str(year) + ')')
+#multiGraph.GetXaxis().SetTitle("Pf MET [GeV]")
+#multiGraph.GetYaxis().SetTitle("R^{out/in} = N^{out} / N^{in}")
+#multiGraph.SetTitle('DY Rin-out scale factor (' + str(year) + ')')
 multiGraph.Draw()
 
 canvas.BuildLegend(0.78, 0.78, 0.9, 0.9)
 canvas.Update()
 canvas.SaveAs("Rinout.png")
+"""
