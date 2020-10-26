519c26cd863363f7095dab250186e46464d2a9e8
cprieels <cprieels@cern.ch>
2020-10-26 14:52:24 +0100

    Bug fixes and improvements

--- scripts/getDYRinout.py
+++ scripts/getDYRinout.py
@@ -95,6 +95,9 @@ outputKappa = {}
 RoutinMC = {}
 errorRoutinMC = {}
 outputRoutinMC = {}
+scaleFactor = {}
+errorScaleFactor = {}
+outputScaleFactor = {}
 
 for channel in channels:
     kappa[channel] = {}
@@ -103,30 +106,48 @@ for channel in channels:
     RoutinMC[channel] = {}
     errorRoutinMC[channel] = {}
     outputRoutinMC[channel] = {}
+    scaleFactor[channel] = {}
+    errorScaleFactor[channel] = {}
+    outputScaleFactor[channel] = {}
 
     for metBin in metBins:
         RoutinMC0bjet = (yieldsMC['0bjet']['out'][channel][metBin]/yieldsMC['0bjet']['in'][channel][metBin])
         errorRoutinMC0bjet = RoutinMC0bjet * math.sqrt((1/yieldsMC['0bjet']['out'][channel][metBin]) + (1/yieldsMC['0bjet']['in'][channel][metBin]))
+
         RoutinData0bjet = (yieldsData['0bjet']['out'][channel][metBin]/yieldsData['0bjet']['in'][channel][metBin])
         errorRoutinData0bjet = RoutinData0bjet * math.sqrt((1/yieldsData['0bjet']['out'][channel][metBin]) + (1/yieldsData['0bjet']['in'][channel][metBin]))
 
         #Statistical error only for now
         kappa[channel][metBin] = RoutinMC0bjet/RoutinData0bjet
-        errorKappa[channel][metBin] = kappa[channel][metBin] *  math.sqrt((errorRoutinMC0bjet/RoutinMC0bjet) ** 2 + (errorRoutinData0bjet/RoutinData0bjet) ** 2)
+        errorKappa[channel][metBin] = kappa[channel][metBin] * math.sqrt((errorRoutinMC0bjet/RoutinMC0bjet) ** 2 + (errorRoutinData0bjet/RoutinData0bjet) ** 2)
         outputKappa[channel][metBin] = str(kappa[channel][metBin]) + " +- " + str(errorKappa[channel][metBin])
 
         RoutinMC[channel][metBin] = kappa[channel][metBin] * (yieldsData['1bjetOrMore']['out'][channel][metBin]/yieldsData['1bjetOrMore']['in'][channel][metBin])
-        errorRoutinMC[channel][metBin] = RoutinMC[channel][metBin] * math.sqrt((1/yieldsData['1bjetOrMore']['out'][channel][metBin]) ** 2 + (1/yieldsData['1bjetOrMore']['in'][channel][metBin]) ** 2 + (errorKappa[channel][metBin]/kappa[channel][metBin]) ** 2)
+        intermediateError = (yieldsData['1bjetOrMore']['out'][channel][metBin]/yieldsData['1bjetOrMore']['in'][channel][metBin]) * math.sqrt((1/yieldsData['1bjetOrMore']['out'][channel][metBin]) + (1/yieldsData['1bjetOrMore']['in'][channel][metBin]))
+        errorRoutinMC[channel][metBin] = RoutinMC[channel][metBin] * math.sqrt((errorKappa[channel][metBin]/kappa[channel][metBin]) ** 2 + (intermediateError/(yieldsData['1bjetOrMore']['out'][channel][metBin]/yieldsData['1bjetOrMore']['in'][channel][metBin])) ** 2)
         outputRoutinMC[channel][metBin] = str(RoutinMC[channel][metBin]) + " +- " + str(errorRoutinMC[channel][metBin])
 
+        numberExpectedOutDYYields = RoutinMC[channel][metBin] * yieldsData['1bjetOrMore']['in'][channel][metBin]
+        errorNumberExpectedOutDYYields = numberExpectedOutDYYields * math.sqrt((errorRoutinMC[channel][metBin]/RoutinMC[channel][metBin]) ** 2 + (1/yieldsData['1bjetOrMore']['in'][channel][metBin]))
+
+        scaleFactor[channel][metBin] = yieldsData['1bjetOrMore']['out'][channel][metBin]/numberExpectedOutDYYields
+        errorScaleFactor[channel][metBin] = scaleFactor[channel][metBin] * math.sqrt((errorNumberExpectedOutDYYields/numberExpectedOutDYYields) ** 2 + (1/yieldsData['1bjetOrMore']['out'][channel][metBin]))
+        outputScaleFactor[channel][metBin] = str(scaleFactor[channel][metBin]) + " +- " + str(errorScaleFactor[channel][metBin])
+
+print("Kappa: ")
 print(outputKappa)
+
+print("RoutinMC: ")
 print(outputRoutinMC)
+
+print("Scale factor: ")
+print(outputScaleFactor)
 
 #==================================================================
 #Plot the results
 
 canvas = r.TCanvas("canvas", "canvas")
-legend = r.TLegend(0.15, 0.74, 0.3, 0.85)
+legend = r.TLegend(0.15, 0.74, 0.35, 0.85)
 r.gStyle.SetOptStat(0)
 
 h = []
@@ -141,11 +162,12 @@ for i, channel in enumerate(channels):
     h[i].SetBins(len(metValues) - 1, array('d', metValues))
 
     for j in range(1, len(metValues)): #Start at 1 to avoid plotting the inclusive MET bin
-        h[i].SetBinContent(j, RoutinMC[channel][str(j)])
-        h[i].SetBinError(j, errorRoutinMC[channel][str(j)])
+        h[i].SetBinContent(j, scaleFactor[channel][str(j)])
+        h[i].SetBinError(j, errorScaleFactor[channel][str(j)])
         h[i].SetLineColor(channelColors[i])
         h[i].SetLineWidth(2)
         h[i].SetMarkerStyle(21)
+        h[i].GetYaxis().SetRangeUser(1.0, 2.2)
         h[i].SetMarkerColor(channelColors[i])
         h[i].Draw("same")
 
